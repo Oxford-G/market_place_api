701b10cfb2d142f4ca6564e84526053240939dac
Enekwechi chinonso gerald <chiiinonsky0009@gmail.com>
2022-06-21 10:52:53 +0100

    Create user model

---
+++ test/models/user_test.rb
@@ -0,0 +1,20 @@
+require "test_helper"
+
+class UserTest < ActiveSupport::TestCase
+  test "user with a valid email should be valid" do
+    user = User.new(email: "test@test.org", password_digest: "test")
+    assert user.valid?
+  end
+
+  test "user with a invalid email should not be valid" do
+    user = User.new(email: "testtest.org", password_digest: "test")
+    assert_not user.valid?
+  end
+
+  test "user with a duplicate email should not be valid" do
+    other_user = users(:one)
+    user = User.new(email: other_user.email, password_digest: "test")
+    assert_not user.valid?
+  end
+  
+end
