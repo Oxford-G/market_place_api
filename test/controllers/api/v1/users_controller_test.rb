092fd53b3b4800fa437ac7c64e634bc409fa6943
Enekwechi chinonso gerald <chiiinonsky0009@gmail.com>
2022-06-23 14:11:41 +0100

    Adds search scopes on the product model

--- test/controllers/api/v1/users_controller_test.rb
+++ test/controllers/api/v1/users_controller_test.rb
@@ -14,8 +14,7 @@ class Api::V1::UsersControllerTest < ActionDispatch::IntegrationTest
     assert_equal @user.email, json_response.dig(:data, :attributes, :email)
     assert_equal @user.products.first.id.to_s, json_response.
                                         dig(:data, :relationships, :products, :data, 0, :id)
-    assert_equal @user.products.first.title, json_response.dig
-                                              (:included, 0, :attributes, :title)
+    assert_equal @user.products.first.title, json_response.dig(:included, 0, :attributes, :title)
   end
 
   test "should create user" do
