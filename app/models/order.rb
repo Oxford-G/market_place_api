0824ea1d210b251ecb66efdebb08f0d6cfdf404e
Enekwechi chinonso gerald <chiiinonsky0009@gmail.com>
2022-06-28 23:54:23 +0100

    add validation for order

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,8 +1,6 @@
 class Order < ApplicationRecord
   belongs_to :user
   before_validation :set_total!
-  # validates :total, numericality: { greater_than_or_equal_to: 0 }
-  # validates :total, presence: true
   has_many :placements, dependent: :destroy
   has_many :products, through: :placements
 
@@ -20,3 +18,6 @@ class Order < ApplicationRecord
     end
   end
 end
+
+  # validates :total, numericality: { greater_than_or_equal_to: 0 }
+  # validates :total, presence: true
